29d0878e902bc6b76007f4447a5e9efc8ad6bd66
john <john@cupist.com>
2019-05-28 13:45:24 +0900

    1. 테스트 코드 틀 Class based -> Function based로 수정
    TODO: Gino initialize 오류 수정 필요

--- apps/users/tests/test_user_repository.py
+++ apps/users/tests/test_user_repository.py
@@ -2,19 +2,22 @@ from apps.users.models import User
 from apps.users.tests import app
 
 
-class TestUserRepository:
-    async def test_save_user(self, app: app):
-        await User.query.gino.all()
-        assert app == 1
+async def test_save_user(app: app):
+    await User.query.gino.all()
+    assert app == 1
 
-    async def test_update_user(self):
-        assert 1 == 1
 
-    async def test_get_user(self):
-        assert 1 == 1
+async def test_update_user():
+    assert 1 == 1
 
-    async def test_get_user_list(self):
-        assert 1 == 1
 
-    async def test_user_login(self):
-        assert 1 == 1
+async def test_get_user():
+    assert 1 == 1
+
+
+async def test_get_user_list():
+    assert 1 == 1
+
+
+async def test_user_login():
+    assert 1 == 1
