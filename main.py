00b084042443c39b98f6c42cce313f9186777b1d
john <john@cupist.com>
2019-05-29 18:41:10 +0900

    1. app.run() 시 CPU코어 개수만큼 worker 실행하도록 수정

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import os
 from sanic import Sanic
 from apps import create_app
 
 
 if __name__ == '__main__':
     app: Sanic = create_app()
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True, workers=os.cpu_count())
